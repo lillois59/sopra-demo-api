90eb2cdde7fe55d4abb11acdc6e6458e28f82232
lillois59 <lillois59119@hotmail.fr>
2026-04-23 05:42:29 +0200

    fix: AdmniController

--- src/main/java/com/example/demo/AdminController.java
+++ src/main/java/com/example/demo/AdminController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import com.example.demo.User;
 import com.example.demo.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,7 @@ public class AdminController {
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "Créer un utilisateur - Réservé ADMIN")
     @PostMapping("/users")
     @PreAuthorize("hasRole('ADMIN')")
     public User createUser(@RequestBody User user) {
