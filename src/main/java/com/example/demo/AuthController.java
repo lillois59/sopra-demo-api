e2f0c4f57f035a5b9e9f3291be081b5d3aecba6a
lillois59 <lillois59119@hotmail.fr>
2026-04-23 07:26:34 +0200

    Recreate AuthController with explicit @CrossOrigin for Railway

--- src/main/java/com/example/demo/AuthController.java
+++ src/main/java/com/example/demo/AuthController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.OPTIONS})
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.POST, RequestMethod.OPTIONS})
 public class AuthController {
 
     private final UserService userService;
